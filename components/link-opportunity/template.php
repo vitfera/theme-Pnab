a0fe8338f5d9c517e8989778c0a68ba55ba43b1a
Victor Ferreira <vitfera@hotmail.com>
2026-07-06 23:45:12 -0300

    remove entidade vinculada

--- components/link-opportunity/template.php
+++ components/link-opportunity/template.php
@@ -10,7 +10,6 @@ use MapasCulturais\i;
 $this->import('
     entity-terms
     mc-link
-    mc-avatar
     mc-popover 
     select-entity
     mc-icon
@@ -19,11 +18,4 @@ $this->import('
 
 <div class="link-opportunity">
     <entity-terms :entity="entity" :editable="true" title="<?php i::_e('Área de Interesse') ?>" taxonomy="area"></entity-terms>
-    <label class="link-opportunity__title bold"><?php i::_e('Entidade Vinculada') ?><br></label>
-    <div class="link-opportunity__ownerEntity">
-        <div class="link-opportunity__header" :class="[entity.ownerEntity.__objectType+'__border', entity.ownerEntity.__objectType+'__color']">
-            <mc-avatar :entity="entity.ownerEntity" size="xsmall"></mc-avatar>
-            {{entity.ownerEntity.name}}
-        </div>
-    </div>
 </div>
